238d8e2fa6f29067c0b3fcb8b659334419aa50e5
ClaySpike <33270607+ClaySpike@users.noreply.github.com>
2019-02-12 11:47:07 -0500

    Dont run this yet ill work on this when i have pc

--- RunTerraria.bat
+++ RunTerraria.bat
@@ -1,8 +1,10 @@
 @echo off
 D:
-cd D:\povin\Documents\My Games\Terraria\Worlds
+cd D:\povin\Documents\My Games\Terraria\Git Worlds\
 git fetch
 git pull origin master
+set /p id=Enter name of the world (CaseSensitive): 
+move D:\povin\Documents\My Games\Terraria\Git Worlds\%id% D:\povin\Documents\My Games\Terraria\Worlds\
 cd D:\Program Files (x86)\Steam\steamapps\common\Terraria
 start /W Terraria.exe
 cd D:\povin\Documents\My Games\Terraria\Worlds
