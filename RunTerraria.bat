1ecb87f95067922cb190780a0eff8a0f13af817b
ClaySpike <povinellimax@gmail.com>
2019-02-11 21:12:39 -0500

    Finish Bat file

--- RunTerraria.bat
+++ RunTerraria.bat
@@ -1,3 +1,4 @@
+@echo off
 D:
 cd D:\povin\Documents\My Games\Terraria\Worlds
 git fetch
@@ -7,6 +8,7 @@ start /W Terraria.exe
 cd D:\povin\Documents\My Games\Terraria\Worlds
 git fetch
 git add .
-set /P /W id=Enter very short discription of what you did: 
+set /p id=Enter very short discription of what you did: 
 git commit -a -m "%id%"
 git push origin master
+pause
