1922d101202084aa2bc7c7ebe49fc89e98f4754d
ClaySpike <povinellimax@gmail.com>
2019-02-11 21:02:56 -0500

    its pizza time

--- RunTerraria.bat
+++ RunTerraria.bat
@@ -6,4 +6,7 @@ cd D:\Program Files (x86)\Steam\steamapps\common\Terraria
 start /W Terraria.exe
 cd D:\povin\Documents\My Games\Terraria\Worlds
 git fetch
+git add .
+set /P id=Enter very short discription of what you did: 
+git commit -a -m %id%
 git push origin master
