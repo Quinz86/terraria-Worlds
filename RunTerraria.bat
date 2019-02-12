2f714e27376e13111bfbe179557fb28c53847468
ClaySpike <povinellimax@gmail.com>
2019-02-11 20:57:10 -0500

    Default Message

---
+++ RunTerraria.bat
@@ -0,0 +1,9 @@
+D:
+cd D:\povin\Documents\My Games\Terraria\Worlds
+git fetch
+git pull origin master
+cd D:\Program Files (x86)\Steam\steamapps\common\Terraria
+start /W Terraria.exe
+cd D:\povin\Documents\My Games\Terraria\Worlds
+git fetch
+git push origin master
